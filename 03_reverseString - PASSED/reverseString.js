de875847367120ca494e3a04993dd70f3efcf5bb
AlexanderTheDev1 <134628095+AlexanderTheDev1@users.noreply.github.com>
2025-03-06 12:13:10 +0200

    3/12 DONE

---
+++ 03_reverseString - PASSED/reverseString.js
@@ -0,0 +1,8 @@
+const reverseString = function(str) {
+    return str.split('').reverse('').join('');
+};
+
+//npm test 03_reverseString
+
+// Do not edit below this line
+module.exports = reverseString;
